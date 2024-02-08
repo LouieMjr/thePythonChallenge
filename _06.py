5641554875305e795be428bf8a2946c029505d0d
Louie Mendez <lmendezjr024@gmail.com>
2024-02-08 15:06:28 +0100

    refactored lvl 6 a bit for better readability

--- _06.py
+++ _06.py
@@ -2,8 +2,12 @@ import urllib.request
 import zipfile
 from io import BytesIO
 
+url = "http://pythonchallenge.com/pc/def/channel.zip"
 zobj = BytesIO()
-zobj.write(urllib.request.urlopen("http://pythonchallenge.com/pc/def/channel.zip").read())
+
+response_bytes = urllib.request.urlopen(url).read()
+zobj.write(response_bytes)
+
 z = zipfile.ZipFile(zobj)
 
 filenum = "90052"
@@ -13,7 +17,7 @@ while True:
     if filenum.isdigit():
         filename = filenum + '.txt'
         comment = z.getinfo(filename).comment.decode('utf-8')  # Decode bytes to str
-        print(comment)
+        # print(comment)
         lcomment.append(comment)
         info = z.read(filename)
         filenum = info.split(b' ')[-1].decode('utf-8')
