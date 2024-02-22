4b578f9600a0d05a2305c19960582626b985016c
Louie Mendez <lmendezjr024@gmail.com>
2024-02-22 22:34:05 +0100

    challenge 7 complete, needed to use chr function to convert numbers to ASCII, then convert remaining numbers in next array a 2nd time

--- _07.py
+++ _07.py
@@ -1,44 +1,24 @@
 from PIL import Image, ImageColor
 from collections import Counter
-
 # from urllib.request import urlopen
+
 # url = "http://www.pythonchallenge.com/pc/def/oxygen.png"
 # img = Image.open(urlopen(url))
 
-
 img = Image.open('oxygen.png')
-# print(img.show())
-# print(img.size)
-# left, upper, right, lower
-box = (0, 45, 608, 50)
-# print(img.show())
-gray_cropped_line = img.crop(box)
-# print(gray_cropped_line.size)
-# print(gray_cropped_line.show())
+y = 48
+end = 607
+step = 7
+pixel_count = []
 
-
-# colors = Counter(gray_cropped_line.getdata())    # dict: color -> number
-
-list_of_RGBvalues = list(gray_cropped_line.getdata())
-list_of_Unique_RGBvalues = set(gray_cropped_line.getdata())
-
-newlist = []
-newlist1 = []
-
-for RGBvalue in list_of_RGBvalues:
-  if(chr(RGBvalue[0]) not in newlist):
-    newlist.append(chr(RGBvalue[0]))
-    # print(RGBvalue, RGBvalue[0], chr(RGBvalue[0]))
+for x in range(0, end, step):
+  # print(img.getpixel((x, y))[0])
+  pixel_count.append(chr(img.getpixel((x, y))[0]))
   
-print(''.join(newlist))
+# print(pixel_count)
+string = ''.join(pixel_count)
+slicedstr = string[-44:-1]
+arrayofString = slicedstr.split(', ')
 
-for x in newlist:
-  if ord(x) not in newlist1:
-
-
-
-# set(colors)                  # set of unique colors  
-# len(colors)                  # number of unique colors 
-# colors[(0, 0, 0)]            # black color frequency
-# newImg = max(colors, key=colors.get)  # most frequent color
-# print(newImg)
+arrayofString = [chr(int(value)) for value in arrayofString]
+print(''.join(arrayofString))
