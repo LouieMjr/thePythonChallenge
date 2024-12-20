039088288b7ae9901a3ecb75a45e47fe9f713098
Louie Mendez <lmendezjr024@gmail.com>
2024-12-20 18:56:34 -0500

    slightly different solution for ch 03

--- _03.py
+++ _03.py
@@ -9,5 +9,23 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 comment = (soup.find_all(string=lambda text: isinstance(text, Comment)))
 
-match = re.findall("[a-z][A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z][a-z]", *comment) # unpack/spread array with *
+pattern = r"[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]"
+match = re.findall(pattern, *comment) # unpack/spread array with *
 print(match) # look at small letter in each of the given strings to makeup answer!
+
+# import re
+# from stringfdf import ascii_lowercase as lowercase
+# from requests import get
+
+# response = get('http://www.pythonchallenge.com/pc/def/equality.html')
+
+# response = str(response.text)
+# pattern = r"[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]"
+# list_of_matches = re.findall(pattern, response)
+
+# small_letters = ''
+# for word in list_of_matches:
+#   for i in range(1, len(word) - 2):
+#     letter = word[i]
+#     if letter in lowercase:
+#       small_letters += letter
