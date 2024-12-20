24b0d0a3aa9297d23b5517628d062fb17d929eac
Louie Mendez <lmendezjr024@gmail.com>
2024-12-20 11:31:21 -0500

    using comprehension instead of mapping object

--- _02.py
+++ _02.py
@@ -1,29 +1,31 @@
-import requests
+import requests, string
 from bs4 import BeautifulSoup, Comment
-
 
 res = requests.get("http://www.pythonchallenge.com/pc/def/ocr.html")
 
 soup = BeautifulSoup(res.text, 'html.parser')
 
-rare = soup.find_all(string=lambda text: isinstance(text, Comment))
+# print(type(soup))
 
-print(rare)
-countObj = {};
-newtxt = '';
+rare = soup.find_all(string=lambda text: isinstance(text, Comment))[1]
+
+print([ele for ele in rare if ele in string.ascii_letters])
+
+# countObj = {};
+# newtxt = '';
 
 
-for element in rare[1]:
-  for symbol in element:
-    if symbol in countObj:
-      countObj[symbol] += 1;
-    else:
-      countObj[symbol] = 1;
+# for element in rare[1]:
+#   for symbol in element:
+#     if symbol in countObj:
+#       countObj[symbol] += 1;
+#     else:
+#       countObj[symbol] = 1;
 
-print(countObj);
+# print(countObj);
 
-for symbol in countObj:
-  if countObj[symbol] == 1:
-    newtxt += symbol;
+# for symbol in countObj:
+#   if countObj[symbol] == 1:
+#     newtxt += symbol;
 
-print(newtxt);
+# print(newtxt);
---
+++ tracebackerror.py
@@ -0,0 +1,24 @@
+import this
+from datetime import datetime, date
+from pytz import timezone
+# import pytz
+# print(pytz.all_timezones)
+
+# format = '%a %b %d %Y %Z %I:%M:%S:%p'
+# switzerland = timezone('Europe/Paris')
+# eastern = timezone('US/Eastern')
+# print(datetime.now(eastern).strftime(format))
+# print(datetime.now(switzerland).strftime(format))
+
+# print(date.today())
+# print(date.today().year)
+# print(date.today().isocalendar()[0])
+# print(date.today().isocalendar)
+
+string = 'racecar'
+# print(string.find('car'))
+
+# while True:
+#     print('Should I stop looping?')
+#     answer = input()
+#     if answer == "yes" or answer == "Yes": break
